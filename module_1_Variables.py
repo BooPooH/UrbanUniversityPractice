4ac77e4d859c85c1cc4159041b06d4b3702d56aa
BooPooH <robot.alfa94@gmail.com>
2024-10-10 18:21:07 +0300

    Create module_1_Variables.py

---
+++ module_1_Variables.py
@@ -0,0 +1,5 @@
+taskCompleted = 12
+timeSpend = 1.5
+courseName = 'Python'
+timeForOneTask = timeSpend/taskCompleted
+print("Курс: ", courseName, ", всего задач:",taskCompleted,", затрачено часов:",timeSpend,", среднее время выполнения",timeForOneTask)
