337abb70831e966046dbcf7aee2806646e25805a
BooPooH <robot.alfa94@gmail.com>
2024-10-18 22:52:27 +0300

    Дополнительное практическое задание по модулю 2

---
+++ module_2_hard.py
@@ -0,0 +1,12 @@
+def find_password(n):
+    result = ""
+    for i in range (1,n):
+        for j in range(i+1,n):
+            if(i+j > n):
+                break
+            if(n % (i+j) == 0):
+                result += str(i) + str(j)
+    return result
+
+for i in range(3,21):
+    print(f"{i} - ", find_password(i))
